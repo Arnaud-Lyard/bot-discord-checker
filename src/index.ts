052e33827336b868dbb678d31d4fb4eb120b8e6a
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-04-09 18:20:12 +0200

    init project

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import Discord from 'discord.js';
+import getConfig from './utils/config';
+import * as dotenv from 'dotenv';
+dotenv.config()
+const config = getConfig();
+import passport from 'passport';
+const BnetStrategy = require('passport-bnet').Strategy;
+import fs from 'fs';
+import path from 'path';
+import express from 'express';
+import { discordLogger } from './utils/logger';
+import Event from './structures/Event'
+import Command from './structures/Command';
+
+export const client = new Discord.Client({
+    intents: [Discord.IntentsBitField.Flags.Guilds]
+});
+
+const app = express();
+
+discordLogger.info('Loading all events...');
+const eventsLoading = (async function loadEvents(dir=path.resolve(__dirname, "./events")) {
+    const files = await fs.promises.readdir(dir);
+    for (const file of files) {
+        const fileDesc = fs.statSync(`${dir}/${file}`);
+
+        if (fileDesc.isDirectory()) {
+            await loadEvents(`${dir}/${file}`);
+            continue;
+        }
+
+        const imported = await import(`${dir}/${file}`);
+        const event: Event = new imported.default();
+        event.register(client);
+        discordLogger.info(`Loaded event ${event.name} (${event.event})`);
+    }
+})();
+
+discordLogger.info("Loading all commands...");
+export const commands = new Discord.Collection<string, Command>(); 
+const cmdsLoading = (async function loadCommands(dir=path.resolve(__dirname, "./commands")) {
+    const files = fs.readdirSync(dir);
+    for (const file of files) {
+        const fileDesc = fs.statSync(dir + "/" + file);
+
+        if(fileDesc.isDirectory()) {
+            await loadCommands(dir + "/" + file );
+            continue;
+        }
+
+        const loadedCommand = await import(dir + "/" + file);
+        const command: Command = new loadedCommand.default();
+
+        commands.set(command.name, command);
+
+        discordLogger.info(`Loaded command ${command.name} from ${file}`);
+    }
+})();
+
+Promise.all([eventsLoading, cmdsLoading]).then(() => {
+    discordLogger.info("Finished loading commands and events.");
+    discordLogger.info(`Connecting to Discord...`);
+    client.login(process.env.DISCORD_TOKEN);
+});
+
+passport.use(new BnetStrategy({
+    clientID: process.env.BNET_CLIENT_ID,
+    clientSecret: process.env.BNET_SECRET,
+    // callbackURL: "https://localhost:3000/oauth/battlenet/callback",
+    region: "eu"
+}, function(accessToken: string, refreshToken: string, profile: string, done:(a: null, b: string) => void): void {
+    return done(null, profile);
+}));
