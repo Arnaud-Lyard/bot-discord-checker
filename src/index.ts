b20780997fce13e2c52199e7d1cd3bdc76ad5ea8
Arnaud-Lyard <arnaud.lyard@gmail.com>
2023-04-16 09:36:28 +0200

    ✨ prepare api connection

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,90 @@
-import Discord from 'discord.js';
-import getConfig from './utils/config';
-import * as dotenv from 'dotenv';
-dotenv.config()
+import Discord from "discord.js";
+import getConfig from "./utils/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 const config = getConfig();
-import passport from 'passport';
-const BnetStrategy = require('passport-bnet').Strategy;
-import fs from 'fs';
-import path from 'path';
-import express from 'express';
-import { discordLogger } from './utils/logger';
-import Event from './structures/Event'
-import Command from './structures/Command';
-import safeConfig from './utils/env';
-import database from './utils/database';
+import fs from "fs";
+import path from "path";
+import express from "express";
+import { discordLogger } from "./utils/logger";
+import Event from "./structures/Event";
+import Command from "./structures/Command";
+import safeConfig from "./utils/env";
+import database from "./utils/database";
 
 export const client = new Discord.Client({
-    intents: [Discord.IntentsBitField.Flags.Guilds]
+  intents: [Discord.IntentsBitField.Flags.Guilds],
 });
 
 const app = express();
 
-discordLogger.info('Loading all events...');
-const eventsLoading = (async function loadEvents(dir=path.resolve(__dirname, "./events")) {
-    const files = await fs.promises.readdir(dir);
-    for (const file of files) {
-        const fileDesc = fs.statSync(`${dir}/${file}`);
+discordLogger.info("Loading all events...");
+const eventsLoading = (async function loadEvents(
+  dir = path.resolve(__dirname, "./events")
+) {
+  const files = await fs.promises.readdir(dir);
+  for (const file of files) {
+    const fileDesc = fs.statSync(`${dir}/${file}`);
 
-        if (fileDesc.isDirectory()) {
-            await loadEvents(`${dir}/${file}`);
-            continue;
-        }
-
-        const imported = await import(`${dir}/${file}`);
-        const event: Event = new imported.default();
-        event.register(client);
-        discordLogger.info(`Loaded event ${event.name} (${event.event})`);
+    if (fileDesc.isDirectory()) {
+      await loadEvents(`${dir}/${file}`);
+      continue;
     }
+
+    const imported = await import(`${dir}/${file}`);
+    const event: Event = new imported.default();
+    event.register(client);
+    discordLogger.info(`Loaded event ${event.name} (${event.event})`);
+  }
 })();
 
 discordLogger.info("Loading all commands...");
-export const commands = new Discord.Collection<string, Command>(); 
-const cmdsLoading = (async function loadCommands(dir=path.resolve(__dirname, "./commands")) {
-    const files = fs.readdirSync(dir);
-    for (const file of files) {
-        const fileDesc = fs.statSync(dir + "/" + file);
+export const commands = new Discord.Collection<string, Command>();
+const cmdsLoading = (async function loadCommands(
+  dir = path.resolve(__dirname, "./commands")
+) {
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fileDesc = fs.statSync(dir + "/" + file);
 
-        if(fileDesc.isDirectory()) {
-            await loadCommands(dir + "/" + file );
-            continue;
-        }
-
-        const loadedCommand = await import(dir + "/" + file);
-        const command: Command = new loadedCommand.default();
-
-        commands.set(command.name, command);
-
-        discordLogger.info(`Loaded command ${command.name} from ${file}`);
+    if (fileDesc.isDirectory()) {
+      await loadCommands(dir + "/" + file);
+      continue;
     }
+
+    const loadedCommand = await import(dir + "/" + file);
+    const command: Command = new loadedCommand.default();
+
+    commands.set(command.name, command);
+
+    discordLogger.info(`Loaded command ${command.name} from ${file}`);
+  }
 })();
 
 Promise.all([eventsLoading, cmdsLoading]).then(() => {
-    discordLogger.info("Finished loading commands and events.");
-    discordLogger.info(`Connecting to Discord...`);
-    client.login(safeConfig.DISCORD_TOKEN);
+  discordLogger.info("Finished loading commands and events.");
+  discordLogger.info(`Connecting to Discord...`);
+  client.login(safeConfig.DISCORD_TOKEN);
 });
 
 try {
-    database.authenticate();
-    discordLogger.info('Database connection has been established successfully.');
-}   catch (error) {
-    discordLogger.info('Unable to connect to the database:', error);
+  database.authenticate();
+  discordLogger.info("Database connection has been established successfully.");
+} catch (error) {
+  discordLogger.info("Unable to connect to the database:", error);
 }
 
-passport.use(new BnetStrategy({
-    clientID: safeConfig.BNET_CLIENT_ID,
-    clientSecret: safeConfig.BNET_SECRET,
-    // callbackURL: "https://localhost:3000/oauth/battlenet/callback",
-    region: "eu"
-}, function(accessToken: string, refreshToken: string, profile: string, done:(a: null, b: string) => void): void {
-    return done(null, profile);
-}));
+let request = fetch(`${safeConfig.BNET_TOKEN_URL}`, {
+  body: new URLSearchParams({ grant_type: `${safeConfig.BNET_GRANT_TYPE}` }),
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    Authorization:
+      "Basic " +
+      Buffer.from(
+        `${safeConfig.BNET_CLIENT_ID}:${safeConfig.BNET_CLIENT_SECRET}`
+      ).toString("base64"),
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
--- src/utils/env.ts
+++ src/utils/env.ts
@@ -14,7 +14,9 @@ interface ENV {
   DISCORD_APPLICATION_ID: string | undefined;
   DISCORD_PUBLIC_KEY: string | undefined;
   BNET_CLIENT_ID: string | undefined;
-  BNET_SECRET: string | undefined;
+  BNET_CLIENT_SECRET: string | undefined;
+  BNET_GRANT_TYPE: string | undefined;
+  BNET_TOKEN_URL: string | undefined;
   DB_PASSWORD: string | undefined;
   DB_USER: string | undefined;
   DB_NAME: string | undefined;
@@ -28,7 +30,9 @@ interface Config {
   DISCORD_APPLICATION_ID: string;
   DISCORD_PUBLIC_KEY: string;
   BNET_CLIENT_ID: string;
-  BNET_SECRET: string;
+  BNET_CLIENT_SECRET: string;
+  BNET_GRANT_TYPE: string;
+  BNET_TOKEN_URL: string;
   DB_PASSWORD: string;
   DB_USER: string;
   DB_NAME: string;
@@ -46,7 +50,9 @@ const getConfig = (): ENV => {
     DISCORD_APPLICATION_ID: process.env.DISCORD_APPLICATION_ID,
     DISCORD_PUBLIC_KEY: process.env.DISCORD_PUBLIC_KEY,
     BNET_CLIENT_ID: process.env.BNET_CLIENT_ID,
-    BNET_SECRET: process.env.BNET_SECRET,
+    BNET_CLIENT_SECRET: process.env.BNET_CLIENT_SECRET,
+    BNET_GRANT_TYPE: process.env.BNET_GRANT_TYPE,
+    BNET_TOKEN_URL: process.env.BNET_TOKEN_URL,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_USER: process.env.DB_USER,
     DB_NAME: process.env.DB_NAME,
